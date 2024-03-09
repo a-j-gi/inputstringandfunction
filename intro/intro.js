10820e5ff3c2626ea5c65091c96ba4bcea0310a6
a-j-gi <157600730+a-j-gi@users.noreply.github.com>
2024-03-09 02:58:25 +0000

    Add files via upload

---
+++ intro/intro.js
@@ -0,0 +1,6 @@
+
+let uname = prompt("Full Name: "); 
+let uage = prompt("Age: ");
+let ustrand = prompt("Strand: ");
+
+println("Hello " + uname + ". Your age is " + uage + ", and your strand is " + ustrand + ".");   
